17e0092f29eeee4405374efe49dc3d9a02bad704
torubanek <torubanek@users.noreply.github.com>
2019-02-12 12:01:04 -0400

    First randDNA

---
+++ src/rdna.hpp
@@ -0,0 +1,15 @@
+ #include <random>
+ 
+char rdna() //This function accepts user input and outputs the DNA sequence
+ {
+	 std::random_device rd;
+	 
+	 char letterb[];
+	 int length;
+	 
+	 std::uniform_int_distribution<> unifrm(1, 4);
+	 
+	 for (int n=0; n<10; n++)
+	 {
+	 `	std::cout<<
+	 
